0a5be13bab831b880b7bd253b7fbf2a681f5f36b
The Binary CPA <physicsphanatic61@gmail.com>
2018-12-18 18:42:12 -0500

    Add Blog Section
    Added a section that gets blog posts from a Web API using javascript and ajax.

---
+++ js/site.js
@@ -0,0 +1,41 @@
+// Class represented in the ViewModel
+var Entry = function(data) {
+    var self = this;
+    self.blogEntryId = data.BlogEntryId;
+    self.timeStamp = data.TimeStamp;
+    self.language = data.Language;
+    self.blogText = data.BlogText;
+    self.likes = ko.observable(data.Likes);
+    self.alreadyLiked = ko.observable(false);
+}
+
+// Create the ViewModel
+var BlogVm = function() {
+    var self = this;
+    self.languages = ['C#', 'JavaScript'];
+    self.chosenLanguageId = ko.observable();
+    self.chosenLanguageData = ko.observableArray([]);
+
+    // Behaviors
+    self.goToLanguage = function(language) {
+        self.chosenLanguageId(language);
+        $.getJSON('https://thebinarycpa.github.io/', {language: language}, function(response) {
+            var mappedEntries = $.map(response, function(item) {return new Entry(item)});
+            self.chosenLanguageData(mappedEntries);
+        });
+    };
+
+    self.liked = function(entry) { 
+        entry.likes(entry.likes() + 1);
+        entry.alreadyLiked(true);
+        $.ajax("https://thebinarycpa.github.io/", {
+            data: ko.toJSON({BlogEntryId: entry.blogEntryId, TimeStamp: entry.timeStamp, Language: entry.language, BlogText: entry.blogText, Likes: entry.likes()}),
+            type: "post", contentType: "application/json",
+            success: function(result) { alert('Thanks for liking my post!') }
+        });
+    };
+
+    self.goToLanguage('C#');
+};
+
+ko.applyBindings(new BlogVm());
